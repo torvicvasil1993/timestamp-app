7d9b403125da07a336d12bad1031d15540759ff0
Victor Silva <victorguidasilva@gmail.com>
2023-03-10 22:23:35 -0300

    Updates app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-""" from flask import Flask
+from flask import Flask
 import datetime
 import os
 
@@ -17,20 +17,6 @@ def hello():
     with open(file_path, 'w') as f:
         f.write(current_timestamp)
     return 'Current timestamp: ' + current_timestamp + '<br/>' + 'Previous timestamp: ' + prev_timestamp
-
-if __name__ == '__main__':
-    app.run()
- """
-
-# app.py
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run()
